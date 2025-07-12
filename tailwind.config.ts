59f85627e4c2449e8092e2b98ba5cbc6478f9fb2
Valentine Aduaka <sabival89@gmail.com>
2025-07-12 12:48:41 -0500

    Setup twin.macro+tailwinds+vite

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
+
+export default config
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [
+		react({
+			babel: {
+				plugins: [
+					'babel-plugin-macros',
+					'babel-plugin-styled-components',
+					'@babel/plugin-proposal-class-properties',
+				],
+			},
+		}),
+		tailwindcss(),
+	],
 })
