adc63f20a4958e7198019a7693245409c1846d7f
Valentine Aduaka <sabival89@gmail.com>
2025-07-12 14:16:46 -0500

    save progress

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import GlobalStyles from './styles/GlobalStyles.tsx'
 import './styles/App.css'
+import GlobalStyles from './styles/GlobalStyles.tsx'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
